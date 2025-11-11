f8c35cf7ef92dd25b4a4f18fce124559c0570655
Андрей Маракулин <annndruha.github@gmail.com>
2025-11-12 01:02:11 +0300

    v1.0.0 Linters

--- main.py
+++ main.py
@@ -9,21 +9,19 @@ import tkinter
 import pystray
 import requests
 from PIL import Image
-from pystray import Menu, MenuItem
 
 
 def resource_path(relative_path):
-    if hasattr(sys, '_MEIPASS'):
+    if hasattr(sys, "_MEIPASS"):
         # If running in a PyInstaller bundle
-        return os.path.join(sys._MEIPASS, relative_path)
-    return os.path.join(os.path.abspath('.'), relative_path)
+        return os.path.join(sys._MEIPASS, relative_path) # noqa
+    return os.path.join(os.path.abspath("."), relative_path)
 
 
 REQUEST_TIMEOUT = 5
-IMG_DIR = resource_path("assets/images")
-PIRATE_FLAG = f"{IMG_DIR}/pirate_flag.png"
+PIRATE_FLAG = resource_path("assets/images/pirate_flag.png")
 
-with open(f"{resource_path('assets')}/cc_to_country.json") as f:
+with open(resource_path("assets/cc_to_country.json")) as f:
     CC_TO_COUNTRY = json.load(f)
 
 
@@ -32,8 +30,8 @@ def ip_prefer_method() -> dict:
     r = requests.get("https://ipinfo.io/json", timeout=REQUEST_TIMEOUT)
     r.raise_for_status()
     data = r.json()
-    data['countryCode'] = data["country"]
-    data['country'] = CC_TO_COUNTRY[data['countryCode']]
+    data["countryCode"] = data["country"]
+    data["country"] = CC_TO_COUNTRY[data["countryCode"]]
     return data
 
 
@@ -65,7 +63,7 @@ class Application:
 
         self.icon = pystray.Icon("My IP in System Tray")
         self.icon.icon = Image.open(PIRATE_FLAG)
-        self.icon.menu = Menu(MenuItem('Quit', lambda: self.quit_window()))
+        self.icon.menu = pystray.Menu(pystray.MenuItem("Quit", lambda: self.quit_window()))
         self.icon.run_detached()
 
         self.thread2 = threading.Thread(target=self.update_data)
@@ -73,7 +71,7 @@ class Application:
         self.root.withdraw()
 
     def quit_window(self):
-        print('Quit by user click')
+        print("Quit by user click")
         self.stop_program = True
         self.icon.icon = None
         self.icon.title = None
@@ -87,8 +85,9 @@ class Application:
                 ip_address = ip["ip"]
                 if ip_address != self.last_ip:
                     self.last_ip = ip_address
-                    self.icon.icon = Image.open(f"{IMG_DIR}\\flags\\{ip['countryCode']}.png")
-                    self.icon.title = ip['country'] + '\n' + ip['city'] + '\n' + ip_address
+                    icon_path = resource_path(f"assets/images/flags/{ip['countryCode']}.png")
+                    self.icon.icon = Image.open(icon_path)
+                    self.icon.title = ip["country"] + "\n" + ip["city"] + "\n" + ip_address
             else:
                 self.last_ip = None
                 self.icon.icon = Image.open(PIRATE_FLAG)
@@ -98,13 +97,13 @@ class Application:
     def run(self):
         try:
             ctypes.windll.shcore.SetProcessDpiAwareness(1)  # if your Windows version >= 8.1
-        except:
+        except: # noqa
             ctypes.windll.user32.SetProcessDPIAware()  # win 8.0 or less
 
         self.root.mainloop()
-        os._exit(42)
+        os._exit(42) # noqa
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = Application()
     app.run()
