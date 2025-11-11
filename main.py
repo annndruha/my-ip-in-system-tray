cb2c54f74473059ea684c2ddd278e410fc6337cd
Андрей Маракулин <annndruha.github@gmail.com>
2025-11-11 23:02:11 +0300

    Only tray

--- main.py
+++ main.py
@@ -11,56 +11,57 @@ import pystray
 from pystray import Icon as Icon, MenuItem as MenuItem
 import darkdetect
 
+
 def resource_path(relative_path):
     if hasattr(sys, '_MEIPASS'):
         # If running in a PyInstaller bundle
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.join(os.path.abspath('.'), relative_path)
 
+
 REQUEST_TIMEOUT = 5
 IMG_DIR = resource_path("assets/images")
 PIRATE_FLAG = f"{IMG_DIR}/pirate_flag.png"
 
+
 class Application:
     def __init__(self):
         self.stop_program = False
-        self.state = 0
+        # self.state = 0
         self.last_ip = None
 
         self.root = Tk()
-        self.root.overrideredirect(True)
+        # self.root.overrideredirect(True)
 
-        self.root.title("MyIP Widget")
-        self.root.iconbitmap(f"{IMG_DIR}\\icon.ico")
+        # self.root.title("MyIP Widget")
+        # self.root.iconbitmap(f"{IMG_DIR}\\icon.ico")
 
         self.detect_theme()
 
-        self.lab1 = Label(self.root, bg=self.bg_color)
-        self.lab1.bind("<Button-3>", self.hide_window)
-        self.lab2 = Label(self.root, bd=0, bg=self.bg_color, fg=self.fg_color, highlightthickness=0, borderwidth=0)
-        self.lab3 = Label(self.root, bd=0, bg=self.bg_color, fg=self.fg_color, highlightthickness=0, borderwidth=0)
+        # self.lab1 = Label(self.root, bg=self.bg_color)
+        # self.lab1.bind("<Button-3>", self.hide_window)
+        # self.lab2 = Label(self.root, bd=0, bg=self.bg_color, fg=self.fg_color, highlightthickness=0, borderwidth=0)
+        # self.lab3 = Label(self.root, bd=0, bg=self.bg_color, fg=self.fg_color, highlightthickness=0, borderwidth=0)
 
-        self.lab1.grid(row=1, column=1)
-        self.lab2.grid(row=2, column=1)
-        self.lab3.grid(row=3, column=1)
+        # self.lab1.grid(row=1, column=1)
+        # self.lab2.grid(row=2, column=1)
+        # self.lab3.grid(row=3, column=1)
 
-        Hovertip(self.lab1, 'right click to close')
+        # Hovertip(self.lab1, 'right click to close')
 
         self.icon = pystray.Icon("ping")
         self.icon.icon = Image.open(PIRATE_FLAG)
         self.icon.run_detached()
         self.icon.menu = (
             MenuItem('Quit', lambda: self.quit_window()),
-            MenuItem('Show', self.show_window)
         )
-        # self.root.wm_attributes("-transparentcolor", self.bg_color)
-        self.root.attributes("-alpha", 0.7)
-        self.root.attributes('-topmost', True)
-        self.root.configure(bg=self.bg_color)
-        self.root.bind("<B1-Motion>", self.move_window)
 
         self.thread2 = threading.Thread(target=self.update_data)
         self.thread2.start()
+        self.root.withdraw()
+
+    def pass_f(self):
+        pass
 
     def detect_theme(self):
         theme = darkdetect.theme()
@@ -72,23 +73,24 @@ class Application:
             self.bg_color = "white"
             self.fg_color = "black"
 
-    def move_window(self, event):
-        self.root.geometry(f'+{event.x_root}+{event.y_root}')
-
+    # def move_window(self, event):
+    #     self.root.geometry(f'+{event.x_root}+{event.y_root}')
+    #
     def quit_window(self):
         self.stop_program = True
         self.icon.icon = None
         self.icon.title = None
         self.icon.stop()
         self.root.destroy()
+    #
+    # def show_window(self):
+    #     self.root.after(0, self.root.deiconify())
+    #
+    # def hide_window(self, event):
+    #     self.root.withdraw()
 
-    def show_window(self):
-        self.root.after(0, self.root.deiconify())
-
-    def hide_window(self, event):
-        self.root.withdraw()
-
-    def find_ip(self):
+    @staticmethod
+    def find_ip() -> bool | dict:
         try:
             req = requests.get("http://ip-api.com/json/", timeout=REQUEST_TIMEOUT)
             if req.status_code == 200:
@@ -105,20 +107,22 @@ class Application:
             ip = self.find_ip()
             if ip:
                 ip_address = ip["query"]
+                country = ip['country']
+                city = ip['city']
                 if ip_address != self.last_ip:
                     self.last_ip = ip_address
-                    self.lab1.image = ImageTk.PhotoImage(image=Image.open(f"{IMG_DIR}\\flags\\{ip['countryCode']}.png"))
-                    self.lab1.config(image=self.lab1.image)
-                    self.lab2.config(text=ip["country"])
-                    self.lab3.config(text=ip_address)
+                    # self.lab1.image = ImageTk.PhotoImage(image=Image.open(f"{IMG_DIR}\\flags\\{ip['countryCode']}.png"))
+                    # self.lab1.config(image=self.lab1.image)
+                    # self.lab2.config(text=ip["country"])
+                    # self.lab3.config(text=ip_address)
                     self.icon.icon = Image.open(f"{IMG_DIR}\\flags\\{ip['countryCode']}.png")
-                    self.icon.title = ip_address
+                    self.icon.title = country + '\n' + city + '\n' + ip_address
             else:
                 self.last_ip = None
-                self.lab1.image = ImageTk.PhotoImage(file=PIRATE_FLAG)
-                self.lab1.config(image=self.lab1.image)
-                self.lab2.config(text="No Internet")
-                self.lab3.config(text="")
+                # self.lab1.image = ImageTk.PhotoImage(file=PIRATE_FLAG)
+                # self.lab1.config(image=self.lab1.image)
+                # self.lab2.config(text="No Internet")
+                # self.lab3.config(text="")
                 self.icon.icon = Image.open(PIRATE_FLAG)
             time.sleep(5)
 
